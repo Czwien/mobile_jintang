2d2288467a3e99b8135ee7ce56717b706ac170c4
Cien <854774326@qq.com>
2020-08-25 18:15:17 +0800

    个人中心页面

--- js/public.js
+++ js/public.js
@@ -1,7 +1,24 @@
 let baseUrl = window.sessionStorage.getItem("baseUrl");
 
+function pui() {
+	this.confirm = function (title,body,bt1,bt2,cllback) {
+		let dialog = "<div class='confirm'>"+
+						"<div class='confirm-content'>"+
+							"<div class='content-title'>"+title+"</div>"+
+							"<div class='content-main-body'>"+body+"</div>"+
+						"</div>"+
+						"<div class='confirm-buttom'>"+
+							"<buttom>"+bt1+"</buttom>"+
+							"<buttom>"+bt2+"</buttom>"+
+						"</div>"+
+					"</div>"
+		$(".mui-content").append(dialog);
+	}
+}
+
 $(function(){
 	
+	// 选择框样式调整
 	if ($(".select-box").length != 0) {
 		// 处理选择框
 		$(".select-box").attr("readonly",true);
@@ -12,4 +29,11 @@ $(function(){
 			
 		});
 	}
-})
+	
+	// 查询框清空
+	if ($(".cancal-img").length != 0) {
+		$(".cancal-img").off().on("click",function() {
+			$(this).prev().val("");
+		});
+	}
+});
