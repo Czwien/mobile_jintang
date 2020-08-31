c465a38e17a7314173ca4c76f90ac34fd9665181
Cien <854774326@qq.com>
2020-08-31 14:41:46 +0800

    advanced query

--- js/public.js
+++ js/public.js
@@ -45,3 +45,39 @@ function fullSelect(obj,selecttor) {
 		$(selecttor).val(selectItems[0].value);
 	});
 }
+
+/**
+ * uuid生成
+ */
+function getUuid() {
+	var s = [];
+	var hexDigits = "0123456789abcdef";
+	for (var i = 0; i < 36; i++) {
+		s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+	}
+	s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+	s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+	s[8] = s[13] = s[18] = s[23] = "-";
+
+	var uuid = s.join("");
+	return uuid;
+}
+
+//将表单转换为JSON
+function serializeJson (selecttor){
+	var serializeObj={};
+	var array=$(selecttor).serializeArray();
+	var str=$(selecttor).serialize();
+	$(array).each(function(){
+		if(serializeObj[this.name]){
+			if($.isArray(serializeObj[this.name])){
+				serializeObj[this.name].push(this.value);
+			}else{
+				serializeObj[this.name]=[serializeObj[this.name],this.value];
+			}
+		}else{
+			serializeObj[this.name]=this.value; 
+		}
+	});
+	return serializeObj;
+};
